c7c5df574502c326c9688b6458f32733e2327b7d
claire froelich <claire.froelich@gmail.com>
2020-06-01 17:26:33 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './mintbean.png';
+import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>App.js</code> is the entrypoint to your app.
         </p>
+        <h2>HAPPY HACKING!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
